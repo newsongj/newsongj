409d635eff50d695b77fc0941874b8b2287a9bd3
Daniel-Doh <danieldos2004@gmail.com>
2026-03-12 23:33:37 +0900

    fix: main.py import 오류 수정

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from database import get_db
-from models import user
+from database import engine, get_db, Base
+import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "deploy skip 오류 수정 test"}
+
 @app.get("/users")
 def get_users(db: Session = Depends(get_db)):
-    return db.query(models.User).all() 
+    return db.query(models.User).all()
